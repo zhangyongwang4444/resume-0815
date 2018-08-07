d16c4968f5806c9ea7761e9cc2ab69689e34bdc7
zhangyongwang <349368992@qq.com>
2018-08-07 23:38:21 +0800

    无刷新留言

--- js/message.js
+++ js/message.js
@@ -1,54 +1,82 @@
-var APP_ID = 'hg9Xg2ojqCnRblDNUbxva3Ba-gzGzoHsz';
-var APP_KEY = '1FfsrmfHjz0LmzNIseyLJHld';
+!function () {
+    var model = {
+        //获取数据
+        init: function () {
+            var APP_ID = 'hg9Xg2ojqCnRblDNUbxva3Ba-gzGzoHsz'
+            var APP_KEY = '1FfsrmfHjz0LmzNIseyLJHld'
+            AV.init({
+                appId: APP_ID,
+                appKey: APP_KEY
+            });
+        },
+        
+        fetch: function () {
+            var query = new AV.Query('Message')
+            return query.find() //Promise 对象 
+        },
+        //创建数据
+        save: function (name, content) {
+            var Message = AV.Object.extend('Message');
+            var message = new Message();
+            return message.save({
+                'name': name,
+                'content': content
+            })                 //Promise 对象
+        }
+    }
+    var view = document.querySelector('section.message')
+    var controller = {
+        model: null,
+        view: null,
+        messageList: null,
+        init: function (view, model) {
+          
+            this.view = view
+            this.model = model
 
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-});
+            this.messageList = view.querySelector('#messageList')
+            this.form = view.querySelector('form')
+            this.model.init()
+            this.loadMessages()
+            this.bindEvents()
+        },
 
-var query = new AV.Query('Message');
-query.find().then(function (messages) {
-    let array = messages.map((items) => items.attributes)
-    array.forEach((item) => {
-        let li = document.createElement('li')
-        li.innerText = `${item.name}:${item.content}`
-        let messageList = document.querySelector('#messageList')
-        messageList.appendChild(li)
-    })
-    // 成功获得实例
-    // todo 就是 id 为 57328ca079bc44005c2472d0 的 Todo 对象实例
-}, function (error) {
-    // 异常处理
-    // 异常处理
-    // 异常处理
+        loadMessages: function () {
+            this.model.fetch().then((messages) => {
+                let array = messages.map((items) => items.attributes)
+                array.forEach((item) => {
+                    let li = document.createElement('li')
+                    li.innerText = `${item.name}:${item.content}`
+                    this.messageList.appendChild(li)
+                })
+                // 成功获得实例
+                // todo 就是 id 为 57328ca079bc44005c2472d0 的 Todo 对象实例
+            }, function (error) {
+                // 异常处理
+                // 异常处理
+                // 异常处理
 
-})
-
-
-let myForm = document.querySelector('#postMessageForm')
-
-myForm.addEventListener('submit', function (e) {
-    e.preventDefault()
-    let content = myForm.querySelector('input[name=content]').value
-    let name = myForm.querySelector('input[name=name]').value
-    var Message = AV.Object.extend('Message');
-    var message = new Message();
-    message.save({
-        'name': name,
-        'content': content
-    }).then(function (object) {
-        let li = document.createElement('li')
-        li.innerText = `${object.attributes.name}:${object.attributes.content}`
-        let messageList = document.querySelector('#messageList')
-        messageList.appendChild(li)
-        let content = myForm.querySelector('input[name=content]').value = ''
-    })
-})
-
-// var TestObject = AV.Object.extend('TestObject');
-// var testObject = new TestObject();
-// testObject.save({
-//     words: 'Hello World!'
-// }).then(function (object) {
-//     alert('LeanCloud Rocks!');
-// })
+            })
+        },
+        bindEvents: function () {
+            this.form.addEventListener('submit', (e) => {
+                e.preventDefault()
+                this.saveMessage()
+            })
+        },
+        saveMessage: function () {
+            let myForm = this.form
+            let content = myForm.querySelector('input[name=content]').value
+            let name = myForm.querySelector('input[name=name]').value
+            this.model.save(name, content).then((object)=> {
+                let li = document.createElement('li')
+                li.innerText = `${object.attributes.name}:${object.attributes.content}`
+                let messageList = document.querySelector('#messageList')
+                messageList.appendChild(li)
+                let content = myForm.querySelector('input[name=content]').value = ''
+            })
+        }
+    }
+    controller.init(view, model)
+    console.log(1)
+}.call()
