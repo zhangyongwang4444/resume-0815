abb3a3984ef40621ab82dbc69521d7cbe5c04668
zhangyongwang <349368992@qq.com>
2018-08-15 07:48:22 +0800

    使用更多命令行

---
+++ src/js/message.js
@@ -0,0 +1,50 @@
+alert(120)
+!function () {
+    let model = Model({ resourceName: 'Message' })
+
+    let view = View('section.message')
+
+    let controller = Controller({
+        init: function (view, model) {
+            this.messageList = view.querySelector('#messageList')
+            this.form = view.querySelector('form')
+            this.loadMessages()
+        },
+        loadMessages: function () {
+            this.model.fetch().then((messages) => {
+                let array = messages.map((items) => items.attributes)
+                array.forEach((item) => {
+                    let li = document.createElement('li')
+                    li.innerText = `${item.name}:${item.content}`
+                    this.messageList.appendChild(li)
+                })
+                // 成功获得实例
+                // todo 就是 id 为 57328ca079bc44005c2472d0 的 Todo 对象实例
+            }, function (error) {
+                // 异常处理
+
+
+            })
+        },
+        bindEvents: function () {
+            this.form.addEventListener('submit', (e) => {
+                e.preventDefault()
+                this.saveMessage()
+            })
+        },
+        saveMessage: function () {
+            let myForm = this.form
+            let content = myForm.querySelector('input[name=content]').value
+            let name = myForm.querySelector('input[name=name]').value
+            this.model.save({ 'name': name, 'content': content }).then((object) => {
+                let li = document.createElement('li')
+                li.innerText = `${object.attributes.name}:${object.attributes.content}`
+                let messageList = document.querySelector('#messageList')
+                messageList.appendChild(li)
+                let content = myForm.querySelector('input[name=content]').value = ''
+            })
+        }
+    })
+    controller.init(view, model)
+    console.log(1)
+}.call()
